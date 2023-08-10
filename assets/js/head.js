c8eafedcfa2586d24178e37d3c718a6a64e119e5
Jose Luis San Martin Barreto <132847890+Inef4ble@users.noreply.github.com>
2023-08-10 14:11:39 -0500

    Delete head.js

--- assets/js/head.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-    $(".menu > ul > li").click(function(e){
-        $(this).siblings().removeClass("active");
-        $(this).toggleClass("active");
-        $(this).find("ul").slideToggle();
-        $(this).siblings().find("ul").slideUp();
-        $(this).siblings().find("ul").find("li").removeClass("active");
-    })
-});
